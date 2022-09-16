0efa7b77f1d530d9e010617bf0fb7805e07b8640
Felipe Souza <felipesouzadsgn@gmail.com>
2022-09-16 19:43:01 -0300

    Adding routes

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,26 @@ import express from "express"
 
 const app = express()
 
-app.get("/ads", (_, res) => {
+app.get("/games", (_, res) => {
+  return res.json([])
+})
+
+app.post("/ads", (_, res) => {
+  return res.status(201).json([])
+})
+
+app.get("/games/:id/ads", (req, res) => {
+  // const gameId = req.params.id
   return res.json([
     { id: 1, name: "Ads 1" },
     { id: 2, name: "Ads 2" },
     { id: 3, name: "Ads 3" },
   ])
+})
+
+app.get("/ads/:id/discord", (req, res) => {
+  // const adId = req.params.id
+  return res.json([])
 })
 
 app.listen(3333)
